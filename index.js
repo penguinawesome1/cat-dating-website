33e9dca351fb0d27dc2f2fa3e93db5609c6d8877
penguinawesome1 <owen.h.colley@gmail.com>
2024-10-27 10:08:18 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ function onMenuClick() {
     ul.classList.toggle(responsive_class_name);
     navbar.classList.toggle(responsive_class_name);
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
