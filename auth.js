6452a36ead0b7331d5be4fb6d70898ff9661a4ad
penguinawesome1 <owen.h.colley@gmail.com>
2024-11-08 13:25:03 -0500

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,33 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBp2Xt-6o12a8aRZBmHJGZtbhHCbda8CAc",
+    authDomain: "fireside-flurt-e55e1.firebaseapp.com",
+    projectId: "fireside-flurt-e55e1",
+    storageBucket: "fireside-flurt-e55e1.firebasestorage.app",
+    messagingSenderId: "662119957266",
+    appId: "1:662119957266:web:cd4020072fd1a637d4a9cb",
+    measurementId: "G-0LP58TPFJE"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+
+const submit = document.getElementById("submit");
+submit.addEventListener("click", function (event) {
+    alert("HII");
+    event.preventDefault(); // stops page from refreshing
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            alert("Creating Account...");
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage);
+        });
+})
