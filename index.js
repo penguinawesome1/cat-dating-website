653b6177c50e4d60cad7a211db703b123bdf4cba
penguinawesome1 <owen.h.colley@gmail.com>
2024-11-11 17:57:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-const auth = getAuth();
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 const track = document.getElementById("profile-track");
 
