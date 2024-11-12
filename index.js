43ccd76aefd54b924ab6eab7983fb1bc14dc44ed
penguinawesome1 <owen.h.colley@gmail.com>
2024-11-12 14:02:58 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -19,29 +19,29 @@ const track = document.getElementById("profile-track");
 const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
 
 const handleOnUp = () => {
-    track.dataset.mouseDownAt = "0";  
+    track.dataset.mouseDownAt = "0";
     track.dataset.prevPercentage = track.dataset.percentage;
 }
 
 const handleOnMove = e => {
-    if(track.dataset.mouseDownAt === "0") return;
-    
+    if (track.dataset.mouseDownAt === "0") return;
+
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-            maxDelta = window.innerWidth / 2;
-    
+        maxDelta = window.innerWidth / 2;
+
     const percentage = (mouseDelta / maxDelta) * -100,
-            nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-            nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -90);
-    
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -90);
+
     track.dataset.percentage = nextPercentage;
-    
+
     track.animate({
         transform: `translate(${nextPercentage}%, -50%)`
     }, { duration: 1200, fill: "forwards" });
-    
-    for(const image of track.querySelectorAll("img")) {
+
+    for (const image of track.querySelectorAll("img")) {
         image.animate({
-        objectPosition: `${100 + nextPercentage}% center`
+            objectPosition: `${100 + nextPercentage}% center`
         }, { duration: 1200, fill: "forwards" });
     }
 }
---
+++ profile.js
@@ -0,0 +1,85 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBp2Xt-6o12a8aRZBmHJGZtbhHCbda8CAc",
+    authDomain: "fireside-flurt-e55e1.firebaseapp.com",
+    projectId: "fireside-flurt-e55e1",
+    storageBucket: "fireside-flurt-e55e1.firebasestorage.app",
+    messagingSenderId: "662119957266",
+    appId: "1:662119957266:web:cd4020072fd1a637d4a9cb",
+    measurementId: "G-0LP58TPFJE"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const user = auth.currentUser;
+
+if (!user) return;
+document.addEventListener('click', (event) => {
+    const target = event.target;
+    switch (target.id) {
+        case "profile-picture":
+            const imageInput = document.getElementById("profile-picture-input");
+            const file = imageInput.files[0];
+            if (!file) break;
+            const reader = new FileReader();
+            let data;
+            reader.onload = (e) => {
+                const profilePicture = document.getElementById("change-profile-picture");
+                profilePicture.src = e.target.result;
+                data = e.target.result;
+            };
+            reader.readAsDataURL(file);
+            updateProfile(user, {
+                photoURL: data
+            }).then(() => {
+                console.log("Profile updated successfully");
+            }).catch((error) => {
+                console.error("Error updating profile:", error);
+            });
+            break;
+        case "change-username":
+            const username = prompt("What is your new username?");
+            if (!username) break;
+            updateProfile(user, {
+                displayName: "New Username"
+            }).then(() => {
+                console.log("Profile updated successfully");
+            }).catch((error) => {
+                console.error("Error updating profile:", error);
+            });
+            break;
+        case "sign-out":
+            signOut(auth).then(() => {
+                console.log("Signed out successfully");
+            }).catch((error) => {
+                console.error("Error signing out:", error);
+            });
+            break;
+        default: break;
+    }
+});
+
+function updateUserProfile(user) {
+    const username = user.displayName;
+    const profilePicture = user.photoURL;
+
+    document.getElementById("username").textContent = username;
+    document.getElementById("profile-picture").src = profilePicture;
+}
+
+onAuthStateChanged(auth, (user) => {
+    document.getElementById("sign-in").classList.toggle("hidden");
+    document.getElementById("profile-picture").classList.toggle("hidden");
+    if (user) {
+        updateUserProfile(user);
+        const uid = user.uid;
+        console.log("User is signed in.");
+    } else {
+        document.getElementById("username").textContent = "Guest";
+        document.getElementById("profile-picture").src = "images/default-profile-picture";
+        console.log("User is signed out.");
+    }
+});
